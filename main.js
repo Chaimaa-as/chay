a7e0f0f70170b4817942d80ffb1c3aa949d3862c
ChaimaaAsri <108627246+Chaimaa-as@users.noreply.github.com>
2023-08-04 14:05:48 +0200

    header

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const primaryHeader = document.querySelector(".homepage-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".navigation");
 
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+});
+
+// const switchElement = document.querySelector(".switch");
+
+// switchElement.addEventListener("click", () => {
+//   document.body.classList.toggle("dark");
+// });
